c3865bb75fea9eeef9e7ef26984e521fa62cd161
mrakkan <93730287+mrakkan@users.noreply.github.com>
2025-10-04 13:41:42 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,9 +4,5 @@ import os
 app = FastAPI()
 
 @app.get("/")
-def root():
-    return {"message": "ok", "port": os.getenv("PORT", "80")}
-
-@app.get("/health")
 def health():
     return {"status": "healthy"}
